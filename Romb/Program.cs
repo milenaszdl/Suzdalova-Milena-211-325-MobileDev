0106d419b603c6c263df18839ec2fc3a3b5bdbbb
milenaszdl <milicca070803@mail.ru>
2023-09-20 23:20:11 +0300

    первое дз с ромбом в консоль

---
+++ Romb/Program.cs
@@ -0,0 +1,49 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        int n = 3; //размер половины ромба
+
+        //первая половина ромба
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n - i; j++)
+            {
+                Console.Write(" ");
+            }
+
+            Console.Write("/");
+
+            for (int j = 2; j < 2 * i; j++)
+            {
+                Console.Write(" ");
+            }
+
+            Console.Write("\\");
+
+            Console.WriteLine();
+        }
+
+
+        //вторая половина ромба
+        for (int i = n ; i >= 1; i--)
+        {
+            for (int j = 1; j <= n - i; j++)
+            {
+                Console.Write(" ");
+            }
+
+            Console.Write("\\");
+
+            for (int j = 2; j < 2 * i; j++)
+            {
+                Console.Write(" ");
+            }
+
+            Console.Write("/");
+
+            Console.WriteLine();
+        }
+    }
+}
