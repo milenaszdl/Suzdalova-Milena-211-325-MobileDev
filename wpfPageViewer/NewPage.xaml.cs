ecbb237c021f5ff1bcedb1c5f46e376f95934354
milenaszdl <milicca070803@mail.ru>
2023-12-25 00:33:13 +0300

    реализованы проверки

--- wpfPageViewer/NewPage.xaml.cs
+++ wpfPageViewer/NewPage.xaml.cs
@@ -63,55 +63,59 @@ namespace wpfPageViewer
         Canvas canvas = new Canvas();
         private void CreateCanvas(object sender, RoutedEventArgs e)
         {
-            string name = PageName.Text;
-            int width = int.Parse(PageWidth.Text);
-            int height = int.Parse(PageHeight.Text);
-            string back = SelectBack.Text;
-            Page page = new Page(name, width, height, back);
-            canvas.Name = page.Name;
-            canvas.Width = page.Width;
-            canvas.Height = page.Height;
-
-            if (back == "С цветами")
+            if (PageName.Text != null && int.TryParse(PageWidth.Text, out int width) && int.TryParse(PageHeight.Text, out int height))
             {
-                ImageBrush imageBrush = new ImageBrush(new BitmapImage(new Uri("C:/Users/milic/source/repos/Suzdalova-Milena-211-325-MobileDev/wpfPageViewer/backgrounds/flowerbackground.jpg"))); // Создаем ImageBrush из выбранного изображения
-                canvas.Background = imageBrush; // Устанавливаем ImageBrush в качестве фона Canvas
-            }
-            else if (back == "С фоторамками")
-            {
-                ImageBrush imageBrush = new ImageBrush(new BitmapImage(new Uri("C:/Users/milic/source/repos/Suzdalova-Milena-211-325-MobileDev/wpfPageViewer/backgrounds/borderback.jpg"))); // Создаем ImageBrush из выбранного изображения
-                canvas.Background = imageBrush; // Устанавливаем ImageBrush в качестве фона Canvas
-            }
-            else if (back == "Книга")
-            {
-                ImageBrush imageBrush = new ImageBrush(new BitmapImage(new Uri("C:/Users/milic/source/repos/Suzdalova-Milena-211-325-MobileDev/wpfPageViewer/backgrounds/bookback.jpg"))); // Создаем ImageBrush из выбранного изображения
-                canvas.Background = imageBrush; // Устанавливаем ImageBrush в качестве фона Canvas
-            }
-            else canvas.Background = Brushes.BurlyWood;
+                string name = PageName.Text;
+                string back = SelectBack.Text;
+                Page page = new Page(name, width, height, back);
+                canvas.Name = page.Name;
+                canvas.Width = page.Width;
+                canvas.Height = page.Height;
 
-            //canvas.MouseLeftButtonUp += Image_MouseLeftButtonUp;
-            //canvas.MouseMove += Image_MouseMove;
+                if (back == "С цветами")
+                {
+                    ImageBrush imageBrush = new ImageBrush(new BitmapImage(new Uri("C:/Users/milic/source/repos/Suzdalova-Milena-211-325-MobileDev/wpfPageViewer/backgrounds/flowerbackground.jpg"))); // Создаем ImageBrush из выбранного изображения
+                    canvas.Background = imageBrush;
+                }
+                else if (back == "С фоторамками")
+                {
+                    ImageBrush imageBrush = new ImageBrush(new BitmapImage(new Uri("C:/Users/milic/source/repos/Suzdalova-Milena-211-325-MobileDev/wpfPageViewer/backgrounds/borderback.jpg"))); // Создаем ImageBrush из выбранного изображения
+                    canvas.Background = imageBrush;
+                }
+                else if (back == "Книга")
+                {
+                    ImageBrush imageBrush = new ImageBrush(new BitmapImage(new Uri("C:/Users/milic/source/repos/Suzdalova-Milena-211-325-MobileDev/wpfPageViewer/backgrounds/bookback.jpg"))); // Создаем ImageBrush из выбранного изображения
+                    canvas.Background = imageBrush;
+                }
+                else canvas.Background = Brushes.BurlyWood;
 
-            Image[] CanvasImages = new Image[3];
-            CanvasImages[0] = new();
-            CanvasImages[1] = new();
-            CanvasImages[2] = new();
-            int j = 0;
-            foreach (Image image in CanvasImages)
-            {
-                image.Source = new BitmapImage(SelectedPics[j].Source);
-                image.Width = (canvas.Width / 3);
-                image.Height = (canvas.Height / 3);
-                //image.MouseLeftButtonDown += Image_MouseLeftButtonDown;
-                Canvas.SetLeft(image, rnd.Next(15, page.Width/2));
-                Canvas.SetTop(image, rnd.Next(15,page.Width/2));
-                canvas.Children.Add(image);
-                j++;
+                //canvas.MouseLeftButtonUp += Image_MouseLeftButtonUp;
+                //canvas.MouseMove += Image_MouseMove;
+
+                Image[] CanvasImages = new Image[3];
+                CanvasImages[0] = new();
+                CanvasImages[1] = new();
+                CanvasImages[2] = new();
+                int j = 0;
+                foreach (Image image in CanvasImages)
+                {
+                    image.Source = new BitmapImage(SelectedPics[j].Source);
+                    image.Width = (canvas.Width / 3);
+                    image.Height = (canvas.Height / 3);
+                    //image.MouseLeftButtonDown += Image_MouseLeftButtonDown;
+                    Canvas.SetLeft(image, rnd.Next(15, page.Width / 2));
+                    Canvas.SetTop(image, rnd.Next(15, page.Width / 2));
+                    canvas.Children.Add(image);
+                    j++;
+                }
+
+
+                PlaceForCanvas.Children.Add(canvas);
             }
-
-
-            PlaceForCanvas.Children.Add(canvas);
+            else MessageBox.Show("Не все параметры заполнены или введены неправильно");
         }
+
+        private Point startPoint;
 
         //private void Image_MouseMove(object sender, MouseEventArgs e)
         //{
@@ -132,7 +136,6 @@ namespace wpfPageViewer
         //    image.ReleaseMouseCapture();
         //}
 
-        //private Point startPoint;
         //private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         //{
         //    Image image = (Image)sender;
@@ -148,37 +151,25 @@ namespace wpfPageViewer
                 {
                     Canvas canvas = (Canvas)element;
 
-                    // Создаем RenderTargetBitmap для Canvas
                     RenderTargetBitmap renderBitmap = new RenderTargetBitmap(
                         (int)canvas.ActualWidth, (int)canvas.ActualHeight,
                         96d, 96d, PixelFormats.Default);
 
-                    // Рендерим Canvas на RenderTargetBitmap
                     renderBitmap.Render(canvas);
 
-                    // Создаем диалоговое окно для выбора пути сохранения файла
-                    Microsoft.Win32.SaveFileDialog saveFileDialog = new Microsoft.Win32.SaveFileDialog();
-                    saveFileDialog.Filter = "PNG Files (*.png)|*.png";
-                    saveFileDialog.DefaultExt = ".png" ;
-                    saveFileDialog.FileName = canvas.Name;
-                    if (saveFileDialog.ShowDialog() == true)
+                    string filePath = $"C:/Users/milic/source/repos/Suzdalova-Milena-211-325-MobileDev/wpfPageViewer/savedpages/{canvas.Name}.png"; // Укажите путь к папке, где нужно сохранить изображение
+                    PngBitmapEncoder encoder = new PngBitmapEncoder();
+                    encoder.Frames.Add(BitmapFrame.Create(renderBitmap));
+
+                    using (FileStream file = File.Create(filePath))
                     {
-                        string filePath = saveFileDialog.FileName;
-
-                        // Создаем кодировщик для сохранения изображения в файл
-                        PngBitmapEncoder encoder = new PngBitmapEncoder();
-                        encoder.Frames.Add(BitmapFrame.Create(renderBitmap));
-
-                        // Сохраняем изображение в выбранный файл
-                        using (FileStream file = File.Create(filePath))
-                        {
-                            encoder.Save(file);
-                        }
+                        encoder.Save(file);
                     }
 
-                    return; // Завершаем функцию после сохранения изображения
+                    MessageBox.Show("Страница сохранена");
+
                 }
-                MessageBox.Show("Страница пуста, нельзя сохранить");
+                else MessageBox.Show("Страница пуста, нельзя сохранить");
             }
         }
     }
