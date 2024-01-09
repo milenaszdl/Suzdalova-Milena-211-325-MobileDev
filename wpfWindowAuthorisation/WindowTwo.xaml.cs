f49590d7e7fecb5ae7d985c3fcb2e79912b66529
milenaszdl <milicca070803@mail.ru>
2024-01-09 06:34:56 +0300

    второе окно доделано

--- wpfWindowAuthorisation/WindowTwo.xaml.cs
+++ wpfWindowAuthorisation/WindowTwo.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace wpfWindowAuthorisation
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для WindowTwo.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class WindowTwo : Window
     {
-        public MainWindow()
+        public WindowTwo()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            WindowTwo Window2 = new WindowTwo();
-            Window2.Show();
         }
     }
 }
