a74cbd882ca82b4a3cf255c9056d092c040cc42b
milenaszdl <milicca070803@mail.ru>
2024-01-11 11:33:59 +0300

    кривые фигуры в пейнте

--- wpfPaint/MainWindow.xaml.cs
+++ wpfPaint/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,6 +32,9 @@ namespace wpfPaint
             PropertyInfo[] colors = BrushColorCombo.ItemsSource.Cast<PropertyInfo>().ToArray();
         }
 
+        //private var drawingAttributes = PaintCanvas.DefaultDrawingAttributes;
+        private DrawingAttributes inkDA = new DrawingAttributes {};
+
         private void BrushSizeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             if (PaintCanvas == null) return;
@@ -39,12 +43,15 @@ namespace wpfPaint
             drawingAttributes.Width = BrushSlider.Value;
             drawingAttributes.Height = BrushSlider.Value;
             PaintCanvas.EraserShape = new EllipseStylusShape(BrushSlider.Value, BrushSlider.Value);
+            inkDA.Width = BrushSlider.Value;
+            inkDA.Height = BrushSlider.Value;
         }
 
         private void BrushColorCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             Color selectedColor = (Color)(BrushColorCombo.SelectedItem as PropertyInfo).GetValue(null, null);
             PaintCanvas.DefaultDrawingAttributes.Color = selectedColor;
+            inkDA.Color = selectedColor;
         }
 
         private void BrushStateCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -78,6 +85,58 @@ namespace wpfPaint
                     pngEncoder.Save(fileStream);
                 }
             }
+        }
+
+        private Point currentPoint;
+        private Point startPoint;
+        private void inkCanvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            startPoint = e.GetPosition(PaintCanvas);
+        }
+
+        private void inkCanvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Point endPoint = e.GetPosition(PaintCanvas);
+            if (e.LeftButton == MouseButtonState.Released)
+            {
+                if (endPoint != startPoint)
+                {
+                    currentPoint = endPoint;
+                }
+            }
+        }
+
+        private void DrawRectangle_Click(object sender, RoutedEventArgs e)
+        {
+            PaintCanvas.DefaultDrawingAttributes = inkDA;
+            var rectAttributes = PaintCanvas.DefaultDrawingAttributes;
+            rectAttributes.Width = BrushSlider.Value;
+            rectAttributes.Height = BrushSlider.Value;
+            Color selectedColor = (Color)(BrushColorCombo.SelectedItem as PropertyInfo).GetValue(null, null);
+            PaintCanvas.DefaultDrawingAttributes.Color = selectedColor;
+            rectAttributes.Color = PaintCanvas.DefaultDrawingAttributes.Color;
+            StylusPointCollection points = new StylusPointCollection();
+            points.Add(new StylusPoint(currentPoint.X, currentPoint.Y));
+            points.Add(new StylusPoint(currentPoint.X + 200, currentPoint.Y));
+            points.Add(new StylusPoint(currentPoint.X + 200, currentPoint.Y + 100));
+            points.Add(new StylusPoint(currentPoint.X, currentPoint.Y + 100));
+            points.Add(new StylusPoint(currentPoint.X, currentPoint.Y));
+            Stroke s = new Stroke(points);
+            PaintCanvas.Strokes.Add(s);
+        }
+
+        private void DrawEllipse_Click(object sender, RoutedEventArgs e)
+        {
+            PaintCanvas.DefaultDrawingAttributes = inkDA;
+            StylusPointCollection points = new StylusPointCollection();
+            for (double theta = 0; theta < 2 * Math.PI; theta += 0.01)
+            {
+                double x = currentPoint.X + 100 * Math.Cos(theta);
+                double y = currentPoint.Y + 50 * Math.Sin(theta);
+                points.Add(new StylusPoint(x, y));
+            }
+            Stroke s = new Stroke(points);
+            PaintCanvas.Strokes.Add(s);
         }
     }
 }
